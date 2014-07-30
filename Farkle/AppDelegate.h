b893f0c0783da9b25a7ca86a814f888cdf3e1d78
Efrén Reyes <efren.reyes@globant.com>
2014-07-30 13:29:34 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Efrén Reyes Torres on 7/30/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
